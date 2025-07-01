e56d027b8604a706b1e326fb96c9a0b242f613bb
sbenshalom <sbenshalom@microsoft.com>
2025-07-01 15:42:49 +0300

    Add mux support and http get endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module torq
 
 go 1.21
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func findCountryHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "Find country endpoint"}`))
+}
 
 func main() {
-	fmt.Println("Hello World")
+	router := mux.NewRouter()
+	router.HandleFunc("/v1/find-country", findCountryHandler).Methods("GET")
+
+	port := ":8080"
+	log.Printf("Server is running on port %s", port)
+	log.Fatal(http.ListenAndServe(port, router))
 }
